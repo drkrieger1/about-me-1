1f33f19ee8c4dd21785aa0c50a32027cbbf307bf
Darryl Davis <ddavis1306@gmail.com>
2017-07-11 17:16:16 -0700

    Finished the html and javascript

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-// 'use strict';
-//
-// var username = prompt('Whats your name?');
-// alert('Hello, ' + username + ' welcome to my page!');
-// alert('Ok ' + username + ' I have a few questions....');
-//
-// //first question
-// var yoyo = prompt('Am I amazing at yo-yoing?');
-// if(yoyo === 'yes'){alert('Thats right!');
-//   console.log('thats right!');
-// }else{alert('Wrong Answer!');
-//   console.log('wrong! im a yoyo champ');
-// }
-// //second question
-// var pie = prompt('Is pumpkin pie my favorite?');
-// if(pie === 'no'){alert('Thats right! Not a fan of pumpkin I LOVE sweet potato pie though!');
-//   console.log('correct');
-// }else{alert('Pumpkin? Gross!!');
-//   console.log('Pumpkin? Gross!!');
-// }
-// //third question
-// var zodiac = prompt('Is my zodiac sign the Scorpio?');
-// if(zodiac === 'yes'){alert('Yea, Obvi...');
-//   console.log('correct');
-// }else{alert('Nah.');
-//   console.log('wrong zodiac');
-//
-// }
-//
-// //fourth question
-// var teeth = prompt('Cavity free since 93\'? ');
-// if(teeth === 'yes'){
-//   console.log('yes');
-//   alert('Yep *insert cool emoji*');
-// }else{alert('Wrong.');
-//   console.log('wrong');
-// }
-//
-// //fifth question
-// var hometown = prompt('Last question, Is my hometown Chicago? ');
-// if(hometown === 'no'){
-//   console.log('yes');
-//   alert('East Side Atlanta Zone 6 ');
-// }else{
-//   console.log('wrong');
-//   alert('Hint: answer is No');
-// }
+'use strict';
+
+var username = prompt('Whats your name?');
+alert('Hello, ' + username + ' welcome to my page!');
+alert('Ok ' + username + ' I have a few questions....');
+
+//first question
+var yoyo = prompt('Am I amazing at yo-yoing?');
+if(yoyo === 'yes'){alert('Thats right!');
+  console.log('thats right!');
+}else{alert('Wrong Answer!');
+  console.log('wrong! im a yoyo champ');
+}
+//second question
+var pie = prompt('Is pumpkin pie my favorite?');
+if(pie === 'no'){alert('Thats right! Not a fan of pumpkin I LOVE sweet potato pie though!');
+  console.log('correct');
+}else{alert('Pumpkin? Gross!!');
+  console.log('Pumpkin? Gross!!');
+}
+//third question
+var zodiac = prompt('Is my zodiac sign the Scorpio?');
+if(zodiac === 'yes'){alert('Yea, Obvi...');
+  console.log('correct');
+}else{alert('Nah.');
+  console.log('wrong zodiac');
+
+}
+
+//fourth question
+var teeth = prompt('Cavity free since 93\'? ');
+if(teeth === 'yes'){
+  console.log('yes');
+  alert('Yep *insert cool emoji*');
+}else{alert('Wrong.');
+  console.log('wrong');
+}
+
+//fifth question
+var hometown = prompt('Last question, Is my hometown Chicago? ');
+if(hometown === 'no'){
+  console.log('yes');
+  alert('East Side Atlanta Zone 6 ');
+}else{
+  console.log('wrong');
+  alert('Hint: answer is No');
+}
